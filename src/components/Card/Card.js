9cf44d9f18186ec1030d8be7a710832d79f0dcde
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-19 16:09:27 -0800

    added CSS

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Card({ pokemon, url_image, height, hp }) {
   return (
     <div className="pokemon-card">
-      <img src={url_image} />
+      <img className='image' src={url_image} />
       <h1>NAME: {pokemon}</h1>
       <p>HEIGHT:{height}</p>
       <p>HP: {hp}</p>

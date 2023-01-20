26061c9e209dcf6c8686fd17dc37a13b844d6a87
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-19 16:14:13 -0800

    added flip cards to CSS

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 export default function Card({ pokemon, url_image, height, hp }) {
   return (
-    <div className="pokemon-card">
-      <img className='image' src={url_image} />
-      <h1>NAME: {pokemon}</h1>
-      <p>HEIGHT:{height}</p>
-      <p>HP: {hp}</p>
+    <div className='flip-card'>
+      <div className='flip-card-inner'>
+        <div className='flip-card-front'>
+          <img className='image' src={url_image} />
+        </div>
+        <div className='flip-card-back'>
+          <div className="pokemon-card">
+            <h1>NAME: {pokemon}</h1>
+            <p>HEIGHT:{height}</p>
+            <p>HP: {hp}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
